f1e97255c5511c373ff7d860ca442594f9e59c8f
Alex Contreras <a.contrerasgranados@gmail.com>
2021-03-09 19:48:56 -0500

    refactor: update environment

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
-  public readonly apiURL = "/ms-api";
+  public readonly apiURL = `${environment.servers.apiURL}/ms-api`;
   public readonly gamePath = "/games"
 
   constructor(private http: HttpClient) { }
